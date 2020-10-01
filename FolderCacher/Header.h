ef6f179887ddf3dc8bb28d6c7a1bc75dfde5b4c4
Riley The Fox <jacobchambers@hotmail.co.uk>
2020-10-01 19:26:41 +0100

    Add project files.

---
+++ FolderCacher/Header.h
@@ -0,0 +1,6 @@
+#ifndef FOLDER_CACHER_H
+#define FOLDER_CACHER_H
+
+void GetFolderSize(string folder, unsigned long long& f_size);
+
+#endif
---
+++ FolderCacher/MainWindow.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "BaseWindow.h"
+
+class MainWindow : public BaseWindow<MainWindow>
+{
+public:
+	PCWSTR ClassName() const;
+	LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
+};
+
